8705ef3c0545169a34b45be0240007289ef5998e
Jara Verbrugghe <jara.verbrugghe@student.howest.be>
2022-04-27 08:53:09 +0200

    models set

--- models/Set.cs
+++ models/Set.cs
@@ -1,19 +1,26 @@
 namespace SpellIt.Models;
 
-public class Set 
+public class Set
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string? Id {get; set;}
+    public string? Id { get; set; }
     public string? Title { get; set; }
     public string? Author { get; set; }
     public string? Language { get; set; }
-    public Word? Words { get; set; }
-    public int? CountOfWords { get{
-        if (Id == null){
-            return null;
-        }else{
-        return Words.Id.Length;
+    public Word[]? Words { get; set; }
+    public int? CountOfWords
+    {
+        get
+        {
+            if (Id == null)
+            {
+                return null;
+            }
+            else
+            {
+                return Words?.Length;
+            }
         }
-    }}
+    }
 }
